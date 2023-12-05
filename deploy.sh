b8db652fa78d1b5e60604390787777b7fba36eda
victorbillaud <victorbillaud@gmail.com>
2023-12-05 10:33:26 +0100

    css working

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,16 @@ kubectl apply -f ./back/backend-deployment.yaml
 kubectl apply -f ./back/backend-service.yaml
 echo "Backend deployed successfully."
 
+echo "Running database migrations with Prisma..."
+# Waiting for the backend pod to be ready
+kubectl wait --for=condition=ready pod -l app=backend --timeout=60s
+
+# Executing prisma migrate deploy in the backend pod
+BACKEND_POD=$(kubectl get pods -l app=backend -o jsonpath="{.items[0].metadata.name}")
+kubectl exec $BACKEND_POD -- yarn prisma migrate deploy
+
+echo "Database migrations applied successfully."
+
 echo "Deploying Frontend..."
 kubectl apply -f ./front/frontend-deployment.yaml
 kubectl apply -f ./front/frontend-service.yaml
--- front/next.config.js
+++ front/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    assetPrefix: '/frontend/',
 }
 
 module.exports = nextConfig
